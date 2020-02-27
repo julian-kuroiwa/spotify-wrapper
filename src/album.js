e9f710068253ff7c61f5b9a884fdf9824aa68ba4
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-02-26 21:15:57 -0300

    add market as param on get albums to get preview_url

--- src/album.js
+++ src/album.js
@@ -1,7 +1,7 @@
 export default function album() {
   return {
-    getAlbum: id => this.request(`${this.apiURL}/albums/${id}`),
+    getAlbum: id => this.request(`${this.apiURL}/albums/${id}?market=ES`),
     getAlbums: ids => this.request(`${this.apiURL}/albums/?ids=${ids}`),
-    getTracks: id => this.request(`${this.apiURL}/albums/${id}/tracks`),
+    getTracks: id => this.request(`${this.apiURL}/albums/${id}/tracks?market=ES`),
   }
 }
